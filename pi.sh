79482d6277e3b1d6f80cc0c19785fc44719a7198
NAGAI Katsuya <katsuya.1128@gmail.com>
2019-11-12 21:31:48 +0900

    [更新] Python3.8のPyInstallerで動作しないので3.7固定にした

--- pi.sh
+++ pi.sh
@@ -5,9 +5,10 @@
 src='.'
 dist='tmp/'
 project='pdfassort'
+pyversion='3.7'
 
 if [ "${project}.py" -nt "dist/${project}.exe" ]; then
     # cp ${src}/${project}.py .
-    pyinstaller --clean --onefile "${project}.py"
+    py -"${pyversion}" -m PyInstaller --clean --onefile "${project}.py"
     # cp "dist/${project}.exe" "${dist}/"
 fi
