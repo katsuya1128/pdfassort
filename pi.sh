4ce2cb320a49cf1302191cc3bd42129a10a389cc
NAGAI Katsuya <katsuya.1128@gmail.com>
2022-04-29 01:46:54 +0900

    Python3.10でPyInstallerが動いているので3.9縛りをやめた

--- pi.sh
+++ pi.sh
@@ -5,10 +5,9 @@
 src='.'
 dist='tmp/'
 project='pdfassort'
-pyversion='3.9'
 
 if [ "${project}.py" -nt "dist/${project}.exe" ]; then
     # cp ${src}/${project}.py .
-    py -"${pyversion}" -m PyInstaller --clean --onefile "${project}.py"
+    py -m PyInstaller --clean --onefile "${project}.py"
     # cp "dist/${project}.exe" "${dist}/"
 fi
